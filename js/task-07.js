c20088867246db53798f0ecbfcc1a5506558dba5
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-05-02 18:03:57 +0300

    Change task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,8 @@
 const inputRangeEl = document.querySelector('#font-size-control')
 const spanTextEl = document.querySelector('#text')
-;
+
 
 
 const onInputChange = inputRangeEl.addEventListener('input', (ivent) => {
    return spanTextEl.style.fontSize =`${ivent.currentTarget.value}px`   
 })
-console.log()
