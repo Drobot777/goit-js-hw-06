286f6f627b165ac06aba9158cb3a8a17e7962bbe
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-05-03 14:36:29 +0300

    Done task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const btnEl = document.querySelector('.change-color')
+console.log()
+const spanEl = document.querySelector('.color')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const onBtnColor = btnEl.addEventListener('click', colorChange )
+
+function colorChange() {
+    spanEl.textContent = getRandomHexColor()
+ document.body.style.background = getRandomHexColor()
 }
