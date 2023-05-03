ade4373d7db0a0f008e35fee0c19cb9f53483a65
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-05-03 13:57:24 +0300

    Done task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formEl = document.querySelector('.login-form')
+
+formEl.addEventListener('submit', onFormSubmit)
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+
+    if (formElements.email.value ==='' || formElements.password.value === '') {
+       return alert('All fields must be filled in')
+    } else {
+        console.log({ email: formElements.email.value, password: formElements.password.value })
+        formEl.reset()
+       }
+
+    }
 
