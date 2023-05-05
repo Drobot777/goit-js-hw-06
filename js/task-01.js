c7e2a03e3a71391b81417142f7e5ac4bb8a7a261
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-05-05 19:29:20 +0300

    Change task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,26 +1,18 @@
-const categoriesEl = document.querySelector('#categories');
+
+
 const itemEl = document.querySelectorAll('.item');
-const titleAnimalsEl = itemEl[0].firstElementChild;
-const elementsAnimalEl = itemEl[0].lastElementChild;
-const titleProductEl = itemEl[1].firstElementChild;
-const elementsProductEl = itemEl[1].lastElementChild;
-const titleTechnologiesEl = itemEl[2].firstElementChild;
-const elementsTechnologiesEl = itemEl[2].lastElementChild;
+console.log(`Number of categories: ${itemEl.length}`)
 
-
-const numberOfCategories = ([...arry]) => {
-    let total = [];
-  arry.forEach((element, index) => {
-   total+= index
+const numberOfCategories = (arry) => {
+  
+  arry.forEach((element) => {
+    console.log(`Category: ${element.firstElementChild.textContent}`)
+    console.log(`Elements: ${element.lastElementChild.children.length}`)
     })
-   return total.length
+   
       
 };
+numberOfCategories(itemEl)
 
-console.log(`Number of categories: ${numberOfCategories(categoriesEl.children)}`)
-console.log(`Category: ${titleAnimalsEl.textContent}`)
-console.log(`Elements: ${numberOfCategories(elementsAnimalEl.children)}`)
-console.log(`Category: ${titleProductEl.textContent}`)
-console.log(`Elements: ${numberOfCategories(elementsProductEl.children)}`)
-console.log(`Category: ${titleTechnologiesEl.textContent}`)
-console.log(`Elements: ${numberOfCategories(elementsTechnologiesEl.children)}`)
+
+
