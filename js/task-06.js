ef2a9a02ecf28656d0481e36b42b4e7fe2f0b316
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-05-06 11:13:34 +0300

    Change task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,19 +3,24 @@ const inputEl = document.querySelector('#validation-input');
 
  const onInputText = inputEl.addEventListener('input', onInputblur)  
 function onInputblur(event) {
-  if (event.currentTarget.value.length > 6) {
+  if (event.currentTarget.value.length < 1 || event.currentTarget.value.length> inputEl.dataset.length) {
+    
     onBlurRed()
   } else {
-    onBlurGreen() 
+    
+    onBlurGreen()
+    
   }
   
 }
 
-function onBlurGreen () {
-  return inputEl.classList.add('valid')
+function onBlurGreen() {
+   inputEl.classList.remove('invalid')
+   inputEl.classList.add('valid')
 }
 function onBlurRed() {
-  return inputEl.classList.add('invalid')
+   inputEl.classList.remove('valid')
+   inputEl.classList.add('invalid')
 }
 
 
