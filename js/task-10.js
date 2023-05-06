1ee0090372639e6876b1326c98ef89052bdc6fe9
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-05-06 14:03:47 +0300

    Change task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,18 +3,16 @@ const divBoxEl = document.querySelector('#boxes')
 const inputEl = document.querySelector('input')
 const creatBtnEl = document.querySelector('[data-create]')
 const destroyBtnEl =document.querySelector('[data-destroy]')
-let namber = 0;
+let namberEnterInput = 0;
+let arrayOfElemebts = [];
 
+const onInputChange = inputEl.addEventListener('input', (event)=>namberEnterInput = event.currentTarget.value)
+const onDestroyPress = destroyBtnEl.addEventListener('click', destroyBoxes)
+const onCreatBtnPress = creatBtnEl.addEventListener('click', () => {
+ createBoxes(namberEnterInput) 
+  divBoxEl.append(...arrayOfElemebts)
+}  )
 
-const onInputChange = inputEl.addEventListener('input', namberInput)
-const onCreatBtnPress = creatBtnEl.addEventListener('click',()=> createBoxes(namber))
-const onDestroyPress = destroyBtnEl.addEventListener('click', ()=> destroyBoxes())
-
-function namberInput(ivent) {
-  
- return namber = ivent.currentTarget.value
-  
-}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -30,14 +28,17 @@ function getRandomHexColor() {
     creatDiv.style.width = `${parametrs}px`
     creatDiv.style.height = `${parametrs}px`
     creatDiv.style.backgroundColor = getRandomHexColor()
-divBoxEl.insertAdjacentElement("beforeend",creatDiv )
+    arrayOfElemebts.push(creatDiv)
   }
 }
 
 function destroyBoxes() {
  divBoxEl.innerHTML =''
-     inputEl.value =''
-  
+  inputEl.value = ''
+ arrayOfElemebts = []
+  namberEnterInput  = 0;
   }
+
+  
 
 
